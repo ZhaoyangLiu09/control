0e9ae2564b88b5bac928e2dce67f7cde61b6da9a
ZhaoyangLiu09 <zhaoyang.liu@student.lut.fi>
2025-09-16 17:15:56 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        document.getElementById('output').innerText = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    document.getElementById('output').innerHTML =
+        "Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude;
+}
+
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            document.getElementById('output').innerText = "User denied the request for Geolocation.";
+            break;
+        case error.POSITION_UNAVAILABLE:
+            document.getElementById('output').innerText = "Location information is unavailable.";
+            break;
+        case error.TIMEOUT:
+            document.getElementById('output').innerText = "The request to get user location timed out.";
+            break;
+        case error.UNKNOWN_ERROR:
+            document.getElementById('output').innerText = "An unknown error occurred.";
+            break;
+    }
+}
